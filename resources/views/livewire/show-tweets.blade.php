f7811af24f3851cd4fdf34b4a13a5c0ce903031d
Matheus Carneiro <matheus10carneiro@gmail.com>
2020-12-06 00:49:03 -0300

    VALIDACAO

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,11 +1,14 @@
 <div>
     Show Tweets
 
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
     <form method="post" wire:submit.prevent="create">
-        <input type="text" name="message" id="message" wire:model="message">
+        <input type="text" name="content" id="content" wire:model="content">
         <button type="submit">Criar Tweet</button>
+        @error('content')
+            {{ $message }}
+        @enderror
     </form>
 
     <hr>
